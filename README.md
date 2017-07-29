36e726e2260e6af74c61a0dde0ab0087827f9fa7
Tommy Thompson <tomthompson828@gmail.com>
2017-07-28 23:33:41 -0400

    reading from ftp and displaying to webpage

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ftp-reader
 Connect to a public ftp and display contents to webapp
+
+This is the ftp...
+https://www.swfwmd.state.fl.us/data/ftp/
+
+Good Stuff...
+https://stackoverflow.com/questions/7968703/is-there-a-public-ftp-server-to-test-upload-and-download
+
+### Local Machine
+`npm install` 
---
+++ server.js
@@ -0,0 +1,55 @@
+// Import FTP Credentials
+var ftpConfig = require('./config.json');
+
+// Node Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var JSFtp = require("jsftp");
+var ftp = new JSFtp(ftpConfig);
+
+
+// Set up Express + Body bodyParser
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve Static elements
+app.use(express.static('public'));
+
+// Set Up Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Basic Index Route
+app.get('/', function(req,res) {
+
+
+  // Read the README.txt file from FTP
+  var str = ""; // Will store the contents of the file 
+  ftp.get('README.txt', function(err, socket) {
+    if (err) return;
+ 
+    socket.on("data", function(d) { str += d.toString(); })
+    socket.on("close", function(hadErr) {
+      if (hadErr){
+        console.error('There was an error retrieving the file.');
+      }
+      else {
+
+        // Render Index Page
+        res.render('index', {hbsObject: str});
+        
+      }
+    });
+    socket.resume();
+  });
+
+
+});
+
+// Open Server
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
